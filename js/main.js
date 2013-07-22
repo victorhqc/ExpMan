6c925f9538dbe352f80949c793946b9ee073c365
victorhqc <victorhqc@gmail.com>
2013-07-21 21:32:38 -0600

    Added functionallity for creating new wallets
    Succesfully list the recently created wallet and creates de new
    IndexedDB database

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 var Template;
 var App;
-
-window.onload = function(){
+(function(){
 	"use strict";
-	Template = new Layout({skin:'templates/web', name:'web'});
-	Template.init(function(){
+	window.onload = function(){
 		//Get the modules
 		new Vi({url:'data/modules.json', response:'json'}).respondo(function(r){
-			buildMenu(r.groups);
-
 			var modules = {};
+			var defaultTemplate = 'templates/web';
 			for(var k in r.groups){
 				if(r.groups.hasOwnProperty(k)){
 					for(var kg in r.groups[k]){
 						if(r.groups[k].hasOwnProperty(kg)){
 							modules[kg] = {};
 							modules[kg].nombre = kg;
-							modules[kg].url = Template.skin+'/modules/';
+							modules[kg].url = defaultTemplate+'/modules/';
 						}
 					}
 				}
@@ -26,106 +23,42 @@ window.onload = function(){
 			App._data = r;
 			App.init(function(){
 				initializeWallets(function(){
-					searchWallets();
-
-					renderWallets();
+					searchWallets(function(){
+						Template = new Layout({skin:defaultTemplate, name:'web'});
+						Template.init(function(){
+						});
+					});
 				});
 			});
 		});
-	});
-};
+	};
 
 
-//Initialize Wallets
-function initializeWallets(callback){
-	"use strict";
-	var mainStructure = App._data.mainStructure;
-	mainStructure.name = 'bank';
-	var bank = DB(mainStructure);
-	bank.initialize(function(){
-		App.bank = bank;
-
-		if(typeof callback === 'function'){
-			callback();
-		}
-	});
-}
-
-//Search for existing wallets
-function searchWallets(){
-	"use strict";
-	var wallets = App.bank.search('wallets', 'by_name', '', 'all');
-	App.wallets = wallets;
-}
-
-//Creates the DOM for wallets
-function renderWallets(){
-	"use strict";
-	var wallets = App.wallets;
-
-	//Blank wallet
-	var j = {container:'#wallet-container', callback:function(){
-		var modal = new Modal();
-	}};
-	var addWallet = new Wallet(j);
-
-	//Add text to the "new-wallet"
-	App.language.translate();
-
-}
-
-function buildMenu(groups){
-	"use strict";
-	var id =  Template.template.menu.getId();
-	var menu = document.getElementById(id);
-
-	var main = '#'+Template.template.main.getId();
-	//main = document.getElementById(main);
-
-	//Building categories
-	for(var g in groups){
-		if(groups.hasOwnProperty(g)){
-			var title = document.createElement('h4');
-			title.setAttribute('data-ltag', g);
-			menu.appendChild(title);
-
-			//Build modules
-			var ul = document.createElement('ul');
-			ul.id = 'sidebarMenu';
-			for(var gm in groups[g]){
-				if(groups[g].hasOwnProperty(gm)){
-					var module = document.createElement('li');
-					module.setAttribute('data-ltag', gm);
-					module.setAttribute('data-module', gm);
-
-					module.addEventListener('click', function(){
-						this.setAttribute('disabled', 'disabled');
-						var elms = menu.getElementsByTagName('li');
-						addClass(this, 'clicked');
-						for(var i = 0, len = elms.length; i < len; i++){
-							if(elms[i] !== this){
-								removeClass(elms[i], 'clicked');
-							}
-						}
-
-						var mod = this.getAttribute('data-module');
-						App.getModule(mod);
-						var js = {
-							'div_id':main
-						};
-						var This = this;
-						App.current.init(js, function(){
-							This.removeAttribute('disabled');
-						});
-					})
-
-					ul.appendChild(module);
-				}
+	//Initialize Wallets
+	function initializeWallets(callback){
+		var mainStructure = App._data.mainStructure;
+		mainStructure.name = 'bank';
+		var bank = DB(mainStructure);
+		bank.initialize(function(){
+			App.bank = bank;
+			if(typeof callback === 'function'){
+				callback();
 			}
-			menu.appendChild(ul);
-		}
+		});
 	}
-}
+
+	//Search for existing wallets
+	function searchWallets(callback){
+		App.bank.search('wallets', 'by_name', '', 'all', function(wallets){
+			App.wallets = wallets;
+			App._wallets = [];
+			if(typeof callback === 'function'){
+				callback();
+			}
+		});	
+	}
+
+})();
 
 //Code thanks to Jake Trent
 //http://rockycode.com/blog/addremove-classes-raw-javascript/
