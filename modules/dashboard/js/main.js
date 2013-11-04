2ce41529c260bd10707ee46c064b09bd39992ac4
victorhqc <victorhqc@gmail.com>
2013-11-04 13:12:45 -0700

    Dashboard now gets activities

--- modules/dashboard/js/main.js
+++ modules/dashboard/js/main.js
@@ -2,9 +2,45 @@
 	"use strict";
 
 	var Module = function(){
-		var j = {container: '#config'};
-		new DateConfig(j);
+
+		var t = this;
+		var j = {
+			container: '#config',
+			choosed: function(dateconfig, data){
+				t.getActivities(data);
+			}
+		};
+
+		this.activities = [];
+
+		this.dateconfig = new DateConfig(j);
+		this.getActivities(this.dateconfig.getData(), function(t){
+
+		});
 	}
+
+	Module.prototype.getActivities = function(date, callback) {
+		callback = (typeof callback === 'function') ? callback : function(){};
+
+		this.date = date;
+
+		var j = {
+			get: 'date',
+			date: this.date
+		}
+
+		var activities = new Activities(j);
+		var t = this;
+		activities.gatherData(function(data){
+			t.activities = data;
+			callback(t);
+		});
+
+	};
+
+	Module.prototype.activitiesTable = function() {
+		
+	};
 
 	var m = new Module();
 })(App);
